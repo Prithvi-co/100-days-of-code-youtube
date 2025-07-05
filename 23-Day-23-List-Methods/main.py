ed1513f00c00c3786022ef438f4017b613bf8972
Prithvi-co <prithvibharadwaj7@gmail.com>
2025-07-05 12:02:28 +0530

    Update main.py

--- 23-Day-23-List-Methods/main.py
+++ 23-Day-23-List-Methods/main.py
@@ -1,15 +1,15 @@
 l = [11, 45, 1, 2, 4, 6, 1, 1]
 print(l)
-# l.append(7)
-# l.sort(reverse=True)
-# l.reverse()
-# print(l.index(1))
-# print(l.count(1))
-# m = l.copy()
-# m[0] = 0
-# l.insert(1, 899)
+# l.append(7)....add 7 at the end of the list
+# l.sort(reverse=True)....sort in descending order
+# l.reverse()....reverse the original list
+# print(l.index(1))....gives the index value of 1 that is 2(agar do 1s hote toh first wale ka inndex deta)
+# print(l.count(1))....ktne baar repeat ho rha h 1 list me i.e.,3
+# m = l.copy()....list k copy bna deta h(nhii to agar m=l hota toh jo changes m me hote woh l me bhi aa jate) 
+# m[0] = 0....(l same rhega)
+# l.insert(1, 899)....899 k list me index 1 hogi
 m = [900, 1000, 1100]
-k = l + m
+k = l + m....l aur m merge ho jayenge(l phle m baad me)
 # print(k)
-# l.extend(m)
+# l.extend(m)....m ko kholo aur l k end me daal do
 print(l)
