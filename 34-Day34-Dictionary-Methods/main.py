ad602927679826b60b1a56f9d5c1fca5aaa29694
Prithvi-co <prithvibharadwaj7@gmail.com>
2025-07-05 16:03:49 +0530

    Update main.py

--- 34-Day34-Dictionary-Methods/main.py
+++ 34-Day34-Dictionary-Methods/main.py
@@ -1,9 +1,9 @@
-ep1 = {122: 45, 123: 89, 567: 69, 670: 69}
+ep1 = {122: 45, 123: 89, 567: 69, 670: 69}     # dictionary is ordered
 ep2 = {222: 67, 566: 90}
 
-# ep1.update(ep2)
-# ep1.clear()
-# ep1.pop(122)
-ep1.popitem()
-del ep1[122]
+# ep1.update(ep2)      #updates ep1 by adding keys values of ep2 in ep1
+# ep1.clear()     #empty dictionary
+# ep1.pop(122)      #remove the key value pair of 122
+ep1.popitem()     #remove last key value pair form ep1
+del ep1[122]       #delete the key value pair of 122
 print(ep1) 
