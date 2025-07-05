d1df2bfbf36fe52b2578752e69e34d4c4a40334e
Prithvi-co <prithvibharadwaj7@gmail.com>
2025-07-05 12:31:38 +0530

    Update main.py

--- 26-Day-26-Exercise-2-Solution/main.py
+++ 26-Day-26-Exercise-2-Solution/main.py
@@ -1,12 +1,12 @@
 import time
 t = time.strftime('%H:%M:%S') 
 hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
+# hour = int(input("Enter hour: "))....takes input from user
 # print(hour)
 
 if(hour>=0 and hour<12):
   print("Good Morning Sir!")
-elif(hour>=12 and hour<17):
+elif(hour>=12 and hour<17):....phython me && use nhi krte and use krte h aur else if use nhi krte elif krte h
   print("Good Afternoon Sir!")
 elif(hour>=17 and hour<0):
   print("Good Night Sir!")
