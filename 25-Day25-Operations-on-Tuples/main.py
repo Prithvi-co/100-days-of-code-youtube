e6852e7f59ab2f9df9b2d637b959777b8496b5cf
Prithvi-co <prithvibharadwaj7@gmail.com>
2025-07-05 12:25:16 +0530

    Update main.py

--- 25-Day25-Operations-on-Tuples/main.py
+++ 25-Day25-Operations-on-Tuples/main.py
@@ -1,7 +1,8 @@
 tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
-# res = tuple1.count(3)
-# res = tuple1.index(3)
-# res = tuple1.index(311)
-# res = tuple1.index(3, 4, 8)
-res = len(tuple1)
+# res = tuple1.count(3)....ktne baar 3 aa rha h
+# res = tuple1.index(3)....phle baar 3 konse index pe aa rha
+# res = tuple1.index(311)....value error
+# res = tuple1.index(3, 4, 8)....slicing the tuple fron 4 to 8 and gives the index of first occurence of 3 in the sliced part i.e.,7
+res = len(tuple1)....length of the tuple
 print('Count of 3 in tuple1 is:', res)
+#tuple me changes hum usko list me convert krke kr skte h aur wapas se tuple me convert kr denge
