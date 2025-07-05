d95428571ac8467ff7863d4e0c8d29991153486c
Prithvi-co <prithvibharadwaj7@gmail.com>
2025-07-05 14:45:11 +0530

    Update main.py

--- 29-Day29-Docstrings/main.py
+++ 29-Day29-Docstrings/main.py
@@ -2,4 +2,4 @@ def square(n):
   '''Takes in a number n, returns the square of n'''
   print(n**2)
 square(5)
-print(square.__doc__)
+print(square.__doc__)      #prints the comments given right before the function definition
